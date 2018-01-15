df156ae887ddaa142aaa7c12d3250358ffb5adb5
John-Mola <johnmmola@gmail.com>
2018-01-15 12:34:31 -0800

    function added

--- Scripts_MasterCopies/02a_colonizeR.R
+++ Scripts_MasterCopies/02a_colonizeR.R
@@ -1,41 +1,46 @@
-# Load the greatest package ever
-require(tidyverse) 
+##%######################################################%##
+#                                                          #
+####             Function to create merged              ####
+####            COLONY output with metadata             ####
+#                                                          #
+##%######################################################%##
 
-# Read in your BestCluster File
-bestCluster = read_table(file = "~/Google Drive/Davis/Projects/RAD Sequencing/testCOLONYoutputs/vsw2m505k1.BestCluster", col_names = TRUE) 
+# Provide function with a .BestCluster output from COLONY, a metadata file with a column named "unique.ID" that matches column "OffspringID" from BestCluster (ostensibly, these things already exist since that's needed for the input to DAT_maker), a name for the output file, and an output directory. 
 
-# Read in your metadata
-metadat = read_csv("~/Google Drive/Davis/Projects/Sierras/Data/2015/sra_matched_all.csv")
+# Run the function
+colonizeR<-function(bestCluster, metadat, projectName, outdir)
+{
+  require(tidyverse)
+  
+  # Trim BestCluster
+  bestC = bestCluster %>% 
+    select(ClusterIndex, OffspringID)
+  
+  # Join, remove individuals absent in BestCluster, create FamilyID column, remove extra variables
+  dat_full <- inner_join(metadat, bestC, by=c("unique.ID"="OffspringID"))  %>% 
+    arrange(ClusterIndex) %>% 
+    mutate(occur = !ClusterIndex %in% ClusterIndex[duplicated(ClusterIndex)]
+    ) %>% 
+    mutate(FamilyID = if_else(occur == TRUE, "s", as.character(ClusterIndex))) #%>%
+    #select(-occur,-gps.ID,-Library.ID,-unique.ID.old,-plate,-occur,-plate.ID)
+  
+  ## Save the dataframe
+  write_csv(dat_full, path = paste0(outdir,projectName,".csv"), col_names = TRUE) 
+  
+}
 
-# Give a name to the output dataset
-projectName = "testcolonizeR"
+# BestCluster from COLONY
+bc = read_table(file = "~/Google Drive/Davis/Projects/RAD Sequencing/testCOLONYoutputs/vsw2m505k1.BestCluster", col_names = TRUE) 
 
-# Output directory
-outdir = "~/Google Drive/Davis/Projects/RAD Sequencing/testCOLONYoutputs/"
+# Metadata
+md = read_csv("~/Google Drive/Davis/Projects/Sierras/Data/2015/sra_matched_all.csv")
 
+# Name the output file
+pn = "testcolonizeR2"
 
-#
-##
-###
-#####
-# Don't mess with stuff below unless you must -- just run it. 
-####
-##
-#
+# Output directory for the final csv
+od = "~/Google Drive/Davis/Projects/RAD Sequencing/testCOLONYoutputs/"
 
+# Run it with objects defined above (change names if desired)
+colonizeR(bc,md,pn,od)
 
-
-# Trim BestCluster
-bestC = bestCluster %>% 
-  select(ClusterIndex, OffspringID)
-
-# Join, remove individuals absent in BestCluster, create FamilyID column, remove extra variables
-dat_full <- inner_join(metadat, bestC, by=c("unique.ID"="OffspringID"))  %>% 
-  arrange(ClusterIndex) %>% 
-  mutate(occur = !ClusterIndex %in% ClusterIndex[duplicated(ClusterIndex)]
-) %>% 
-  mutate(FamilyID = if_else(occur == TRUE, "s", as.character(ClusterIndex))) %>%
-  select(-occur,-gps.ID,-Library.ID,-unique.ID.old,-plate,-occur,-plate.ID)
-
-## Save the dataframe
-write_csv(dat_full, path = paste0(outdir,projectName,".csv"), col_names = TRUE) 
